6440678b5576072a8a71c1380b589499b3e5043b
heinwaiyankyaw <heinwaiyankyaw2001@gmail.com>
2023-08-15 21:17:34 +0630

    CompleteSetUpTemplate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <div className="card">
+        <form action="" className="calculator">
+          <div className="display">
+            <input type="text" />
+          </div>
+          <div>
+            <input type="button" value="AC" />
+            <input type="button" value="DE" />
+            <input type="button" value="." />
+            <input type="button" value="/" />
+          </div>
+          <div>
+            <input type="button" value="7" />
+            <input type="button" value="8" />
+            <input type="button" value="9" />
+            <input type="button" value="*" />
+          </div>
+          <div>
+            <input type="button" value="4" />
+            <input type="button" value="5" />
+            <input type="button" value="6" />
+            <input type="button" value="+" />
+          </div>
+          <div>
+            <input type="button" value="1" />
+            <input type="button" value="2" />
+            <input type="button" value="3" />
+            <input type="button" value="-" />
+          </div>
+          <div>
+            <input type="button" value="00" />
+            <input type="button" value="0" />
+            <input type="button" value="=" className="equal" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
